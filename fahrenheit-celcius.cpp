dfd2685d4d13333d8085b3d5359a41dfc2f52d5f
kipbonnie <113593108+kipbonnie@users.noreply.github.com>
2022-10-06 12:20:23 +0300

    Add files via upload

---
+++ fahrenheit-celcius.cpp
@@ -0,0 +1,13 @@
+//program to convert degrees to fahnreheit
+    #include<iostream>
+    using namespace std;
+
+	int main()   
+                   {   
+                   float fahrenheit, celcius;  
+                   cout<<"Enter temperature in fahrenheit";
+                   cin>>fahrenheit;
+                   celcius =(fahrenheit-32)*5/9;
+				   cout<<"Temperature in celcius is"<<celcius;  
+                       return (0);  
+    }  
